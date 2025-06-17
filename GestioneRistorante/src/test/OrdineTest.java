7f9944d9afc1aef752aaaab9ee15fe1b18e27a8e
Luigi Cirillo <luigicirillo23@gmail.com>
2025-06-17 19:13:23 +0200

    Rimosso il test commentato 'testCalcolaConto' dalla classe OrdineTest

--- GestioneRistorante/src/test/OrdineTest.java
+++ GestioneRistorante/src/test/OrdineTest.java
@@ -65,41 +65,4 @@ public class OrdineTest {
         assertEquals(i, 0);
         assertEquals(nuovo_stato, ordine.getStato());
     }
-
-    // @Test
-    // public void testCalcolaConto() {
-    //     int idOrdine = 10;
-    //     EntityOrdine ordine = new EntityOrdine(idOrdine, 1, 5, "in_attesa");
-    //     ordine.scriviSuDB();
-    //     EntityPietanza pietanza1 = new EntityPietanza(190, "Pizza Margherita", 7.5, 2);
-    //     EntityPietanza pietanza2 = new EntityPietanza(191, "Spaghetti Carbonara", 8.0, 1);
-    //     EntityPietanza pietanza3 = new EntityPietanza(192, "Tiramisu", 5.0, 1);
-    //     pietanza1.scriviSuDB(0); // Usa 0 per generare un nuovo ID
-    //     pietanza2.scriviSuDB(0);
-    //     pietanza3.scriviSuDB(0);
-    //     System.out.println("Pietanza inserita con ID: " + pietanza1.getIdPietanza());
-    //     System.out.println("Pietanza inserita con ID: " + pietanza2.getIdPietanza());
-    //     System.out.println("Pietanza inserita con ID: " + pietanza3.getIdPietanza());
-    //     int quantita = 1;
-    //     boolean risultatoAggiunta = ordine.aggiungiPietanza(pietanza1, quantita);
-    //     boolean risultatoAggiunta2 = ordine.aggiungiPietanza(pietanza2, quantita);
-    //     boolean risultatoAggiunta3 = ordine.aggiungiPietanza(pietanza3, quantita);
-    //     System.out.println("Risultato aggiunta pietanza 1: " + risultatoAggiunta);
-    //     System.out.println("Risultato aggiunta pietanza 2: " + risultatoAggiunta2);
-    //     System.out.println("Risultato aggiunta pietanza 3: " + risultatoAggiunta3);
-    //     System.out.println("Costo totale dell'ordine dopo aggiunta: " + ordine.getCostoTotale());
-    //     EntityRistorante ristorante = new EntityRistorante(1);
-    //     double costoCoperto = ristorante.getCostoCoperto();
-    //     System.out.println("Costo coperto per persona: " + costoCoperto);
-    //     double costoPrevistoSenzaCoperto = pietanza1.getPrezzo() * quantita;
-    //     costoPrevistoSenzaCoperto += pietanza2.getPrezzo() * quantita;
-    //     costoPrevistoSenzaCoperto += pietanza3.getPrezzo() * quantita;
-    //     System.out.println("Costo previsto senza coperto: " + costoPrevistoSenzaCoperto);
-    //     double costoPrevistoConCoperto = costoPrevistoSenzaCoperto + (costoCoperto * ordine.getNumPersone());
-    //     System.out.println("Costo previsto con coperto: " + costoPrevistoConCoperto);
-    //     boolean includiCoperto = true;
-    //     double costoEffettivo = ordine.calcolaConto(includiCoperto);
-    //     System.out.println("Costo effettivo calcolato: " + costoEffettivo);
-    //     assertEquals(33.00, costoEffettivo, 0);
-    // }
 }
